1b1316bb4b61a3e102f63700db46f20994f32919
Victor Hugo <victorblog410@gmail.com>
2021-06-16 23:03:47 -0300

    using accessories on pytest

--- tests/test_ex1.py
+++ tests/test_ex1.py
@@ -1,5 +1,16 @@
 import pytest
 
+@pytest.fixture(scope="session")
+def fixture_1():
+    print('run-fixture-1')
+    return 1
 
-def test_example():
-    assert 1 == 1
+
+def test_example1(fixture_1):
+    num = fixture_1
+    assert num == 1
+
+def test_example2(fixture_1):
+    num = fixture_1
+    assert num == 1
+
